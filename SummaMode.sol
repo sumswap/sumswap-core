ddba0280f79d0b2a1fc2060528e2e43403c1ccb8
SumSwap <78221130+sumswap@users.noreply.github.com>
2021-03-17 17:19:45 +0800

    update for the contracts about price and decimals

--- SummaMode.sol
+++ SummaMode.sol
@@ -1,12 +1,12 @@
-
 pragma solidity ^0.6.0;
 
-import "./interfaces/IERC20Sumswap.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
 
+interface IDecimals{
+    function decimals() external view returns (uint8);
+}
 abstract contract SummaPriContract is AccessControl {
 
     function node_count() external virtual returns (uint256);
@@ -57,49 +57,27 @@ contract SummaMode is Ownable {
     mapping(address => uint256) public _priBalances;
 
     mapping(address => uint256) public _pubBalances;
-    //  --------------------------produce------------------------
-    //    PlaceType public priPlaceType = PlaceType({
-    //    token : address(0),
-    //    price : 0.5 * 10000,
-    //    duration : 60 * 24 * 225,
-    //    minTurnOver : 10000,
-    //    placeFeeRate : 0.2 * 1000
-    //    });
-    //
-    //    uint256 public pub_count = 3000;
-    //    // 16M summa
-    //    uint256 public pub_total_summa = 16000000;
-    //
-    //    PlaceType public pubPlaceType = PlaceType({
-    //    token : address(0),
-    //    price : 0.75 * 10000,
-    //    duration : 90 * 24 * 225,
-    //    minTurnOver : 4000,
-    //    placeFeeRate : 0.2 * 1000
-    //    });
-    // ----------------------produce--------------------------
 
-    //  --------------------------dev------------------------
     PlaceType public priPlaceType = PlaceType({
     token : address(0),
     price : 0.5 * 10000,
-    duration : 2 * 225,
+    duration : 60 * 24 * 225,
     minTurnOver : 10000,
     placeFeeRate : 0.2 * 1000
     });
 
-    uint256 public pub_count = 12;
+    uint256 public pub_count = 3000;
     // 16M summa
-    uint256 public pub_total_summa = 64000;
+    uint256 public pub_total_summa = 16000000;
 
     PlaceType public pubPlaceType = PlaceType({
     token : address(0),
     price : 0.75 * 10000,
-    duration : 3 * 225,
+    duration : 90 * 24 * 225,
     minTurnOver : 4000,
     placeFeeRate : 0.2 * 1000
     });
-    // ----------------------dev--------------------------
+
     bool public pub_switch = false;
 
     uint256 public pub_time = 0;
@@ -125,18 +103,18 @@ contract SummaMode is Ownable {
 
     function pubBalanceOf(address account) public view returns (uint256) {
         if ((!pub_switch || block.number > pub_time.add(pubPlaceType.duration) || pub_count <= 0) && pubTokenAmount > 0) {
-            uint256 realPrice = pubTokenAmount.div(pub_total_summa).mul(10000).div(10 ** uint256(IERC20Sumswap(pubPlaceType.token).decimals()));
+            uint256 realPrice = pubTokenAmount.div(pub_total_summa).mul(10000).div(10 ** uint256(IDecimals(pubPlaceType.token).decimals()));
             if (realPrice > pubPlaceType.price) {
-                return _pubBalances[account].mul(10 ** uint256(IERC20Sumswap(summa).decimals())).div(pubTokenAmount.div(pub_total_summa));
+                return _pubBalances[account].mul(10 ** uint256(IDecimals(summa).decimals())).div(pubTokenAmount.div(pub_total_summa));
             }
-            return _pubBalances[account].mul(10 ** uint256(IERC20Sumswap(summa).decimals())).div(pubPlaceType.price.mul(10 ** uint256(IERC20Sumswap(pubPlaceType.token).decimals())).div(10000));
+            return _pubBalances[account].mul(10 ** uint256(IDecimals(summa).decimals())).div(pubPlaceType.price.mul(10 ** uint256(IDecimals(pubPlaceType.token).decimals())).div(10000));
         }
         return 0;
     }
 
     function realPrice() public view returns (uint256){
         if ((!pub_switch || block.number > pub_time.add(pubPlaceType.duration) || pub_count <= 0) && pubTokenAmount > 0) {
-            return pubTokenAmount.div(pub_total_summa).mul(10000).div(10 ** uint256(IERC20Sumswap(pubPlaceType.token).decimals()));
+            return pubTokenAmount.div(pub_total_summa).mul(10000).div(10 ** uint256(IDecimals(pubPlaceType.token).decimals()));
         }
         return 0;
     }
@@ -180,25 +158,25 @@ contract SummaMode is Ownable {
     /*to approve token before use this func*/
     function privatePlacement(uint256 amount) public {
         require(amount > 0, "amount must be gt 0");
-        require(IERC20Sumswap(priPlaceType.token).allowance(_msgSender(), address(this)) > 0, "allowance not enough");
-        //        require(IERC20Sumswap(priPlaceType.token).balanceOf(_msgSender()) >= IERC20Sumswap(priPlaceType.token).allowance(_msgSender(), address(this)), "balance not enough");
+        require(IERC20(priPlaceType.token).allowance(_msgSender(), address(this)) > 0, "allowance not enough");
+        //        require(IERC20(priPlaceType.token).balanceOf(_msgSender()) >= IERC20(priPlaceType.token).allowance(_msgSender(), address(this)), "balance not enough");
         bool fullNode = SummaPriContract(summaPri).getRoleMemberCount(NODE_ROLE) >= SummaPriContract(summaPri).node_count() ? true : false;
-        uint256 actualTurnOver = priPlaceType.minTurnOver * 10 ** uint256(IERC20Sumswap(priPlaceType.token).decimals());
+        uint256 actualTurnOver = priPlaceType.minTurnOver * 10 ** uint256(IDecimals(priPlaceType.token).decimals());
         if (!hasPri[_msgSender()] && !(Address.isContract(_msgSender())) && !fullNode && SummaPriContract(summaPri).node_switch() && block.number <= SummaPriContract(summaPri).node_time().add(priPlaceType.duration) && actualTurnOver <= amount) {
             if (SummaPriContract(summaPri).addNode(_msgSender())) {
-                _priBalances[_msgSender()] = priPlaceType.minTurnOver.mul(10000).div(priPlaceType.price).mul(10 ** uint256(IERC20Sumswap(summa).decimals()));
+                _priBalances[_msgSender()] = priPlaceType.minTurnOver.mul(10000).div(priPlaceType.price).mul(10 ** uint256(IDecimals(summa).decimals()));
                 hasPri[_msgSender()] = true;
                 if (SummaPriContract(summaPri).getRoleMemberCount(NODE_ROLE) >= SummaPriContract(summaPri).node_count()) {
                     pub_switch = true;
                     pub_time = block.number;
                 }
             }
-            IERC20Sumswap(priPlaceType.token).transferFrom(_msgSender(), SummaPriContract(summaPri).nodeReceiveAddress(), amount);
+            SafeERC20.safeTransferFrom(IERC20(priPlaceType.token),_msgSender(), SummaPriContract(summaPri).nodeReceiveAddress(), amount);
         } else {
-            if (amount >= (minBackAmount * 10 ** uint256(IERC20Sumswap(priPlaceType.token).decimals()))) {
-                IERC20Sumswap(priPlaceType.token).transferFrom(_msgSender(), SummaPriContract(summaPri).nodeReceiveAddress(), amount.mul(priPlaceType.placeFeeRate).div(1000));
+            if (amount >= (minBackAmount * 10 ** uint256(IDecimals(priPlaceType.token).decimals()))) {
+                SafeERC20.safeTransferFrom(IERC20(priPlaceType.token),_msgSender(), SummaPriContract(summaPri).nodeReceiveAddress(), amount.mul(priPlaceType.placeFeeRate).div(1000));
             } else {
-                IERC20Sumswap(priPlaceType.token).transferFrom(_msgSender(), SummaPriContract(summaPri).nodeReceiveAddress(), amount);
+                SafeERC20.safeTransferFrom(IERC20(priPlaceType.token),_msgSender(), SummaPriContract(summaPri).nodeReceiveAddress(), amount);
             }
         }
     }
@@ -207,16 +185,16 @@ contract SummaMode is Ownable {
     /*to approve token before use this func*/
     function publicPlacement(uint256 amount) public {
         require(amount > 0, "amount must be gt 0");
-        require(IERC20Sumswap(pubPlaceType.token).allowance(_msgSender(), address(this)) > 0, "allowance not enough");
-        require(IERC20Sumswap(pubPlaceType.token).allowance(_msgSender(), address(this)) >= amount, "please approve allowance for this contract");
+        require(IERC20(pubPlaceType.token).allowance(_msgSender(), address(this)) > 0, "allowance not enough");
+        require(IERC20(pubPlaceType.token).allowance(_msgSender(), address(this)) >= amount, "please approve allowance for this contract");
         require(SummaPriContract(summaPri).node_time() > 0, "not start");
-        //        require(IERC20Sumswap(pubPlaceType.token).balanceOf(_msgSender()) >= IERC20Sumswap(pubPlaceType.token).allowance(_msgSender(), address(this)), "balance not enough");
+        //        require(IERC20(pubPlaceType.token).balanceOf(_msgSender()) >= IERC20(pubPlaceType.token).allowance(_msgSender(), address(this)), "balance not enough");
         bool closeNode = (block.number >= SummaPriContract(summaPri).node_time().add(priPlaceType.duration) || SummaPriContract(summaPri).getRoleMemberCount(NODE_ROLE) >= SummaPriContract(summaPri).node_count()) ? true : false;
         if (closeNode && !pub_switch) {
             pub_switch = true;
             pub_time = block.number;
         }
-        uint256 actualTurnOver = pubPlaceType.minTurnOver * 10 ** uint256(IERC20Sumswap(pubPlaceType.token).decimals());
+        uint256 actualTurnOver = pubPlaceType.minTurnOver * 10 ** uint256(IDecimals(pubPlaceType.token).decimals());
         if (!(Address.isContract(_msgSender())) && closeNode && block.number <= pub_time.add(pubPlaceType.duration) && pub_switch && pub_count > 0 && actualTurnOver <= amount) {
             if (SummaPriContract(summaPri).hasRole(PUBLIC_ROLE, _msgSender())) {
                 if (_pubBalances[_msgSender()] <= 0) {
@@ -229,12 +207,12 @@ contract SummaMode is Ownable {
                 pubTokenAmount = pubTokenAmount.add(amount);
                 _pubBalances[_msgSender()] = _pubBalances[_msgSender()].add(amount);
             }
-            IERC20Sumswap(pubPlaceType.token).transferFrom(_msgSender(), address(this), amount);
+            SafeERC20.safeTransferFrom(IERC20(pubPlaceType.token),_msgSender(), address(this), amount);
         } else {
-            if (amount < (minBackAmount * 10 ** uint256(IERC20Sumswap(priPlaceType.token).decimals()))) {
-                IERC20Sumswap(priPlaceType.token).transferFrom(_msgSender(), address(this), amount);
+            if (amount < (minBackAmount * 10 ** uint256(IDecimals(priPlaceType.token).decimals()))) {
+                SafeERC20.safeTransferFrom(IERC20(priPlaceType.token),_msgSender(), address(this), amount);
             } else {
-                IERC20Sumswap(pubPlaceType.token).transferFrom(_msgSender(), address(this), amount.mul(pubPlaceType.placeFeeRate).div(1000));
+                SafeERC20.safeTransferFrom(IERC20(pubPlaceType.token),_msgSender(), address(this), amount.mul(pubPlaceType.placeFeeRate).div(1000));
             }
         }
     }
@@ -245,7 +223,7 @@ contract SummaMode is Ownable {
     }
 
     function withdrawToken(address addr) public onlyOwner {
-        IERC20Sumswap(addr).transfer(_msgSender(), IERC20Sumswap(addr).balanceOf(address(this)));
+        SafeERC20.safeTransfer(IERC20(addr),_msgSender(), IERC20(addr).balanceOf(address(this)));
     }
 
     receive() external payable {
